980d4c4727a500eca065a3c6df4a0576086a8d66
pietrykovsky <m.pietrykowski2001@gmail.com>
2025-03-08 15:12:43 +0100

    Add Prisma commands to package.json and refactor NavBar component for better authentication handling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,47 +5,55 @@ import { useSession, signOut } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-export default function NavBar() {
-  const { data } = useSession();
+function AuthButtons() {
+  const { data, isPending } = useSession();
   const currentUser = data?.user;
   const router = useRouter();
+  if (isPending) {
+    return <div className="flex flex-center gap-2 items-center"></div>;
+  }
+  return (
+    <div className="flex flex-center gap-2 items-center">
+      {currentUser ? (
+        <>
+          <Link className="text-white hover:text-zinc-400" href="/dashboard">
+            Dashboard
+          </Link>
+          <Button
+            variant={"destructive"}
+            onClick={async () =>
+              await signOut({
+                fetchOptions: {
+                  onSuccess: () => {
+                    router.push("/");
+                  },
+                },
+              })
+            }
+          >
+            Logout
+          </Button>
+        </>
+      ) : (
+        <>
+          <Link href="/login">
+            <Button variant="secondary">Login</Button>
+          </Link>
+          <Link href="/register">
+            <Button variant="secondary">Register</Button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
 
+export default function NavBar() {
   return (
     <nav className="w-full bg-zinc-950 z-10 shadow-md sticky top-0">
       <div className="flex justify-between lg:justify-around items-center px p-4 text-center gap-2">
         <h1 className={`font-semibold text-xl text-white hover:text-gray-300`}>Forum Engine</h1>
-        <div className="flex flex-center gap-2 items-center">
-          {currentUser ? (
-            <>
-              <Link className="text-white hover:text-zinc-400" href="/dashboard">
-                Dashboard
-              </Link>
-              <Button
-                variant={"destructive"}
-                onClick={async () =>
-                  await signOut({
-                    fetchOptions: {
-                      onSuccess: () => {
-                        router.push("/");
-                      },
-                    },
-                  })
-                }
-              >
-                Logout
-              </Button>
-            </>
-          ) : (
-            <>
-              <Link href="/login">
-                <Button variant="secondary">Login</Button>
-              </Link>
-              <Link href="/register">
-                <Button variant="secondary">Register</Button>
-              </Link>
-            </>
-          )}
-        </div>
+        <AuthButtons />
       </div>
     </nav>
   );
